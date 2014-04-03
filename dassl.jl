2f1cd45df8209c14ab2cb79d24a0b49898481a41
Paweł Biernat <pawel.biernat@gmail.com>
2014-04-03 18:43:00 +0200

    Cleaned up the arguments of modified_newton

--- dassl.jl
+++ dassl.jl
@@ -1,11 +1,82 @@
 module dassl
 
-export modified_newton, predictor
+export modified_newton, predictor, dummy
+
+function dummy(t,y,h,F,rtol,atol)
+    k=length(h)-1
+
+    alphas=-sum([1/j for j=1:k])
+    t_next=t+h[end]
+    (y0,dy0)=predictor(h,y)
+
+    F_y =jacobian(x->F(t_next, x,dy0), y0)
+    F_dy=jacobian(x->F(t_next,y0,  x),dy0)
+
+    a=-alphas/h[end]
+    b=dy0-a*y0
+    jac=(a*F_dy+F_y)
+
+    (status,yn)=modified_newton( x->(-jac\F(t_next,x,a*x+b)), y0, v->dassl_norm(v,y0,rtol,atol) )
+end
+
+
+function modified_newton(f,y0,norm)
+
+    # first guess comes from the predictor method, then we compute the
+    # second guess to get the norm1
+
+    delta=f(y0)
+    norm1=norm(delta)
+    yn=y0+delta
+
+    # after the first iteration the norm turned out to be very small,
+    # terminate and return the first correction step
+
+    if norm1 < 10*eps(norm1)
+        status=0
+        return(status,yn)
+    end
+
+    # maximal number of iterations is set by dassl algorithm to 4
+
+    for i=1:3
+
+        delta=f(yn)
+        normn=norm(delta)
+        rho=(normn/norm1)^(1/i)
+        yn=yn+delta
+
+        # iteration failed to converge
+
+        if rho > 9/10
+            status=-1
+            return(status,y0)
+        end
+
+        err=rho/(1-rho)*normn
+
+        # iteration converged successfully
+
+        if err < 1/3
+            info("Converged after i=" * string(i) * " iterations with estimated error=" * string(err))
+            status=0
+            return(status,yn)
+        end
+
+    end
+
+    # unable to converge after 4 iterations
+
+    status=-1
+    return(status,y0)
+end
+
 
 # h=[         h_{n-k+1},    ...   ,                h_{n},       h_{n+1}] --- length = k+1
 # y=[y_{n-k},            y_{n-k+1}, ... , y_{n-1},        y_{n}        ] --- length = k+1
 # k is the order of BDF
 # size(h)=k+2, size(y)=k+1
+# for reference see p. 119
 function predictor(h,y)
     if size(y,2) != length(h)
         error("Incompatible size of h and y")
@@ -24,56 +95,6 @@ function predictor(h,y)
     y0       = sum(phi_star,2)[:,1]
     dy0      = sum(hcat([g[i]*phi_star[:,i] for i=1:k]...),2)[:,1]
     return(y0,dy0)
-end
-
-function modified_newton(t,y,h,F,rtol,atol)
-    k=length(h)-1
-    alphas=-sum([1/j for j=1:k])
-    t_next=t+h[end]
-    (y0,dy0)=predictor(h,y)
-
-    a=-alphas/h[end]
-    b=dy0-a*y0
-
-    F_y =jacobian(x->F(t_next, x,dy0), y0)
-    F_dy=jacobian(x->F(t_next,y0,  x),dy0)
-    jac=(a*F_dy+F_y)
-
-    # first guess comes from the predictor method, then we compute the
-    # second guess to get the norm ||y0-y1||
-    delta=-jac\F(t_next,y0,a*y0+b)
-    norm1=dassl_norm(delta,y0,rtol,atol)
-    # next step
-    yn=y0+delta
-
-    # after the first iteration the norm turned out to be very small,
-    # terminate and return the first correction step
-    if norm1 < 10*eps(norm1)
-        return(yn)
-    end
-
-    # maximal number of iterations is set by dassl algorithm to 4
-    for i=1:4
-        delta=-jac\F(t_next,yn,a*yn+b)
-        normn=dassl_norm(delta,yn,rtol,atol)
-        rho=(normn/norm1)^(1/i)
-        yn=yn+delta
-
-        conv_factor=rho/(1-rho)*normn
-
-        # decide weather to quit the iteration
-        # 1) iteration converged successfully
-        if conv_factor < 1/3
-            println("Converged after i=" * string(i) * " iterations with convergence factor=" * string(conv_factor))
-            return(yn)
-        # 2) iteration failed to converge
-        elseif conv_factor > 9/10
-            error("Unable to converge!")
-            return(y0)
-        end
-    end
-
-    return(yn)
 end
 
 
--- dassl_test.jl
+++ dassl_test.jl
@@ -3,11 +3,17 @@ using dassl
 # number of previously known steps
 k=5
 dh=0.1
-
-t=float([j*dh for j=0:k])
-y=hcat(map(x->[exp(x)],t)...)
-h=[diff(t), dh]
+rtol=0.001
+atol=0.001
 
 function F(t,y,dy)
     return(dy-y)
 end
+
+function sol(t)
+    exp(t)
+end
+
+t=float([j*dh for j=0:k])
+y=hcat(map(sol,t)...)
+h=[diff(t), dh]
