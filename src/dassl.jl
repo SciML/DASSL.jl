d875b910e38b345c75918cae27d93354da30cb90
Paweł Biernat <pawel.biernat@gmail.com>
2014-05-04 22:27:14 +0200

    Included the contents of the InterPoly package

--- src/dassl.jl
+++ src/dassl.jl
@@ -599,4 +599,64 @@ function jac_delta{T<:Number}(y0     :: Vector{T}
     return d
 end
 
+
+# returns the value of the interpolation polynomial at the point x0
+function interpolateAt{T<:Number,N}(x::Vector{T}, y::AbstractArray{T,N}, x0::T)
+
+    if length(x)!=size(y,ndims(y))
+        error("x and y have to be of the same size.")
+    end
+
+    n = length(x)
+    p = zero(T)
+
+    for i=1:n
+        Li =one(T)
+        dLi=zero(T)
+        for j=1:n
+            if j==i
+                continue
+            else
+                Li*=(x0-x[j])/(x[i]-x[j])
+            end
+        end
+        p.+=Li.*y[[[1:size(y,n) for n=1:N-1],i]...]
+    end
+    return p
+end
+
+# returns the value of the derivative of the interpolation polynomial
+# at the point x0
+function interpolateDerivativeAt{T<:Number,N}(x::Vector{T}, y::AbstractArray{T,N}, x0::T)
+
+    if length(x)!=size(y,ndims(y))
+        error("x and y have to be of the same size.")
+    end
+
+    n = length(x)
+    p = zero(T)
+
+    for i=1:n
+        dLi=zero(T)
+        for k=1:n
+            if k==i
+                continue
+            else
+                dLi1=one(T)
+                for j=1:n
+                    if j==k || j==i
+                        continue
+                    else
+                        dLi1*=(x0-x[j])/(x[i]-x[j])
+                    end
+                end
+                dLi+=dLi1/(x[i]-x[k])
+            end
+        end
+        p.+=dLi.*y[[[1:size(y,n) for n=1:N-1],i]...]
+    end
+    return p
+end
+
+
 end
