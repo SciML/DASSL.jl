145521014b5cbcb94cfc7eaa26b4bdbbe638638c
Paweł Biernat <pawel.biernat@gmail.com>
2014-05-06 09:07:41 +0200

    Updated test.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,8 @@ F(t,y,dy)=(dy+y)
 sol(t)=exp(-t)
 
 const y0=[sol(0.0)]
-const tspan=[0.0, 1.0]
+const tspan=[0.0, 10.0]
 
-(tn,yn)=driver(F,y0,tspan, rtol = 1.0e-3, atol = 1.0e-5, h0 = 1.0e-4)
+(tn,yn)=dassl.dasslSolve(F,y0,tspan, rtol = 1.0e-3, atol = 1.0e-5, h0 = 1.0e-4)
 
-@test abs(yn[1,end]-sol(tn[end]))/sol(tn[end]) < 0.02
+@test abs(yn[1,end]-sol(tn[end]))/sol(tn[end]) < 0.002
