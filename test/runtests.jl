3dafb92db50bf6b53b68fc2e859f5de6152e9e18
Paweł Biernat <pawel.biernat+github@gmail.com>
2015-02-09 09:43:06 +0100

    Added a new test for the minimal allowable torlerances

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,58 +1,67 @@
 using DASSL
 using Base.Test
+using FactCheck
 
-F(t,y,dy)=(dy+y.^2)
-Fy(t,y,dy)=diagm(2y)
-Fdy(t,y,dy)=eye(length(y))
+facts("Testing maxorder") do
 
-sol(t)=1./(1 .+ t)
-const tspan=[0.0, 1.0]
+    F(t,y,dy)=(dy+y.^2)
+    Fy(t,y,dy)=diagm(2y)
+    Fdy(t,y,dy)=eye(length(y))
 
-atol = 1.0e-5
-rtol = 1.0e-3
+    sol(t)=1./(1 .+ t)
+    tspan=[0.0, 1.0]
+
+    atol = 1.0e-5
+    rtol = 1.0e-3
 
 
-# test the maxorder option
-for order=1:6
-    # scalar version
-    (tn,yn,dyn)=DASSL.dasslSolve(F, sol(0.0), tspan, maxorder = order)
-    aerror = maximum(abs(yn-sol(tn)))
-    rerror = maximum(abs(yn-sol(tn))/abs(sol(tn)))
-    nsteps = length(tn)
+    # test the maxorder option
+    for order=1:6
+        # scalar version
+        (tn,yn,dyn)=DASSL.dasslSolve(F, sol(0.0), tspan, maxorder = order)
+        aerror = maximum(abs(yn-sol(tn)))
+        rerror = maximum(abs(yn-sol(tn))/abs(sol(tn)))
+        nsteps = length(tn)
 
-    @test aerror < 2*nsteps*atol
-    @test rerror < 2*nsteps*rtol
+        @fact aerror => less_than(2*nsteps*atol)
+        @fact rerror => less_than(2*nsteps*rtol)
 
-    # vector version
-    (tnV,ynV,dynV)=DASSL.dasslSolve(F,[sol(0.0)], tspan, maxorder = order)
+        # vector version
+        (tnV,ynV,dynV)=DASSL.dasslSolve(F,[sol(0.0)], tspan, maxorder = order)
 
-    @test  vcat(ynV...)==yn
-    @test vcat(dynV...)==dyn
+        @fact  vcat(ynV...) => yn
+        @fact vcat(dynV...) => dyn
 
-    # analytical jacobian version (vector)
-    (tna,yna,dyna)=DASSL.dasslSolve(F, [sol(0.0)], tspan, maxorder = order, Fy = Fy, Fdy = Fdy)
-    aerror = maximum(abs(map(first,yn)-sol(tn)))
-    rerror = maximum(abs(map(first,yn)-sol(tn))/abs(sol(tn)))
-    nsteps = length(tn)
+        # analytical jacobian version (vector)
+        (tna,yna,dyna)=dasslSolve(F, [sol(0.0)], tspan, maxorder = order, Fy = Fy, Fdy = Fdy)
+        aerror = maximum(abs(map(first,yn)-sol(tn)))
+        rerror = maximum(abs(map(first,yn)-sol(tn))/abs(sol(tn)))
+        nsteps = length(tn)
 
-    @test aerror < 2*nsteps*atol
-    @test rerror < 2*nsteps*rtol
+        @fact aerror => less_than(2*nsteps*atol)
+        @fact rerror => less_than(2*nsteps*rtol)
+    end
 end
 
-# test the initial derivatives y0 using the van der Pol equation
+facts("Testing minimal error tolerances") do
+    eps=1e-6
+    # van der Pol equation
+    Fvdp(t,y,dy)=([dy[1]+y[2],
+                   eps*dy[2]-y[1]+y[2]*(1.0-y[1]^2)])
+    y0 = [2., 0]
+    tn = (0.,[2.,0.],[0.,-2/eps])
 
-# van der Pol equation
-Fvdp(t,y,dy)=([dy[1]+y[2],
-               eps*dy[2]-y[1]+(y[2]^3/3-y[2])])
+    tol = 1e0
+    while true
+        tol/=10
+        sol=dasslIterator(Fvdp, y0, 1.0; reltol=tol, abstol=tol)
 
-eps = 1e-8
-y0  = [ 0.0,       1]
-dy0 = [-1.0, 2/3/eps]
+        try
+            (tn,yn,dyn)=consume(sol)
+        catch
+            break
+        end
 
-# this should result in a warning and fail to start the intergration
-# (tn,yn)=DASSL.dasslSolve(Fvdp, y0, tspan, rtol = rtol, atol = atol, h0 = 1.0e-4)
-# @test length(tn)==1
-
-# it should work after specifying the initial derivative
-(tn,yn)=DASSL.dasslSolve(Fvdp, y0, tspan, dy0 = dy0)
-@test length(tn)>1
+    end
+    @fact tol => roughly(1e-19)
+end
